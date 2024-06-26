098603a6bc5bf25e6f5c548c53c464ad1bdea68a
elkadhi-oussama <elkadhioussama@yahoo.com>
2024-06-26 13:35:57 +0100

    get all movies

--- client/src/components/PartMovies/MovieCard.js
+++ client/src/components/PartMovies/MovieCard.js
@@ -1,33 +1,24 @@
-import React from 'react'
-import "./MovieCard.css"
+import React from "react";
+import "./MovieCard.css";
 
-const MovieCard = () => {
+const MovieCard = ({ movie }) => {
   return (
     <div>
-        <div class="wrapperFree">
-        <div class="cardFree">
-        <img src="" alt="" />
-            <div class="descriptionsFree">
-                <h1>John Wick 3</h1>
-                <p>
-                    After gunning down a member of the High Table -- the shadowy international assassin's guild -- legendary hit man John Wick finds himself stripped of the organization's protective services. Now stuck with a $14 million bounty on his head, Wick must fight his way through the streets of New York as he becomes the target of the world's most ruthless killers.
-                </p>
-                <button>
-                    <i class="fab fa-youtube"></i>
-                    Play trailer on YouTube
-                </button>
-            </div>
+      <div className="cardFree">
+        <img src={movie.image} alt="" />
+        <div className="descriptionsFree">
+          <h1> {movie.title} </h1>
+          <p>
+           {movie.description}
+          </p>
+          <button>
+            <i className="fab fa-youtube"></i>
+            Play trailer on YouTube
+          </button>
         </div>
-        
+      </div>
     </div>
+  );
+};
 
-
-
-
-
-
-    </div>
-  )
-}
-
-export default MovieCard
+export default MovieCard;
--- client/src/components/PartMovies/MovieList.js
+++ client/src/components/PartMovies/MovieList.js
@@ -1,26 +1,34 @@
-import React, { useEffect, useState } from 'react'
-import MovieCard from './MovieCard'
-import axios from "axios"
+import React, { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
+import axios from "axios";
 const MovieList = () => {
-  const [movies, setmovies] = useState([])
+  const [movies, setmovies] = useState([]);
   useEffect(() => {
-  const getAllMovies = async ()=>{
-    try {
-     const res = await axios.get("https://movies-application-api.vercel.app/movie/getAllMovie")
-     console.log(res)
-    } catch (error) {
-      console.log("we have some error when trying to get all movies : ", error)
-    }
-  }  
-  getAllMovies()
-  
-  }, [])
-  
-  return (
-    <div>
-        <MovieCard />
-    </div>
-  )
-}
+    const getAllMovies = async () => {
+      try {
+        const res = await axios.get(
+          "https://movies-application-api.vercel.app/movie/getAllMovie"
+        );
+        setmovies(res.data.getAllMovie);
+      } catch (error) {
+        console.log(
+          "we have some error when trying to get all movies : ",
+          error
+        );
+      }
+    };
+    getAllMovies();
+  }, []);
 
-export default MovieList
+  return (
+    <div className="wrapperFree">
+      {movies ? (
+        movies.map((movie) => <MovieCard movie={movie} key={movie._id} />)
+      ) : (
+        <h1>Coming soon...</h1>
+      )}
+    </div>
+  );
+};
+
+export default MovieList;
