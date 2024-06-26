ba2ec499d1d3ee036904d1b374f6439059c9b905
elkadhi-oussama <elkadhioussama@yahoo.com>
2024-06-26 12:01:19 +0100

    deploy

--- API/index.js
+++ API/index.js
@@ -32,11 +32,13 @@ const app = express();
 app.use(express.json());
 
 // Enable Cross-Origin Resource Sharing (CORS) for all requests
-app.use(cors({
-  origin:["https://movies-application-api.vercel.app"], // Specify the allowed origin
-  methods:["POST", "GET", "DELETE","PUT"], // Specify the allowed HTTP methods
-  credentials:true // Allow cookies to be sent with requests
-}));
+app.use(
+  cors({
+    origin: ["https://movies-application-front.vercel.app/"], // Specify the allowed origin
+    methods: ["POST", "GET", "DELETE", "PUT"], // Specify the allowed HTTP methods
+    credentials: true, // Allow cookies to be sent with requests
+  })
+);
 
 // Use Body-Parser's URL-encoded middleware to parse URL-encoded request bodies
 app.use(bodyParser.urlencoded({ extended: false }));
